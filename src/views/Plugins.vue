2226f2a4c229b202e3bd90c35b4f32a8ffd556bf
Soli <asoliman96@gmail.com>
2023-07-09 23:36:02 +0200

    feat: improve plugins listing and add search

--- src/views/Plugins.vue
+++ src/views/Plugins.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="animated fadeIn">
     <BackButton backgroundColor="#1E90FF" />
-    <div class="plugin-card-container" v-for="plugin in plugins" :key="plugin.id">
-      <div class="plugin-card">
-        <div class="animated fadeIn plugin-content">
-          <img :src="plugin.logo_url" alt="Plugin Logo" class="cover">
-          <div class="plugin-detail">
-            <div><strong>{{plugin.name_for_human}}</strong></div>
-            <div class="subtitle">{{plugin.description_for_human}}</div>
-            <p class="author"> - {{plugin.name_for_model}}</p>
+    <div class="search-input-container margin-auto">
+      <input type="text" v-model="searchTerm" placeholder="Search..." class="search-input">
+    </div>
+    <div class="padding-sm margin-auto" v-for="plugin in filteredPlugins" :key="plugin.id">
+      <div class="card padding-sm margin-auto animated fadeIn">
+        <div class="horizontal center">
+          <div class="horizontal center h-space-sm">
+            <img :src="plugin.logo_url" alt="Logo" class="logo">
+            <h4>{{plugin.name_for_human}}</h4>
           </div>
+          <label class="switch">
+            <input type="checkbox" v-model="plugin.showModelDescription">
+            <span class="slider"></span>
+          </label>
+        </div>
+        <p>
+          {{plugin.showModelDescription ? plugin.description_for_model : plugin.description_for_human}}
+        </p>
+        <div class="h-space-sm">
+          <a :href="plugin.domain" target="_blank">Domain</a>
+          <a :href="plugin.contact_email" target="_blank">Email</a>
+          <a :href="plugin.legal_info_url" target="_blank">Legal</a>
         </div>
       </div>
     </div>
@@ -26,58 +39,129 @@ export default {
     BackButton,
   },
   data() {
-    return { plugins };
+    return {
+      plugins: plugins.map(plugin => ({ ...plugin, showModelDescription: false })),
+      searchTerm: ''
+    };
   },
+  computed: {
+    filteredPlugins() {
+      const lowerCaseSearchTerm = this.searchTerm.toLowerCase();
+      return this.plugins.filter(plugin => {
+        return plugin.name_for_human.toLowerCase().includes(lowerCaseSearchTerm) ||
+          plugin.description_for_human.toLowerCase().includes(lowerCaseSearchTerm) ||
+          (plugin.description_for_model && plugin.description_for_model.toLowerCase().includes(lowerCaseSearchTerm));
+      });
+    }
+  }
 };
 </script>
 
 <style scoped>
-.plugin-card-container {
-  margin: auto;
-  padding: 3vh;
-}
-
-.plugin-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.search-input-container {
   max-width: 500px;
-  margin: auto;
-  padding: 3vh;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  font-size: 1.2em;
-  background-color: #fff;
-  transition: transform .75s;
+  justify-content: center;
+}
+.search-input {
+  width: 100%;
+  padding: 1vh;
+  font-size: 1.5em;
+  border-radius: 5px;
+  border: 1px solid #ccc;
 }
 
-.plugin-card:hover {
+.padding-sm {
+  padding: 3vh;
+}
+
+.horizontal {
+  display: flex;
+}
+
+.center {
+  align-items: center;
+  justify-content: space-between;
+}
+
+.margin-auto {
+  margin: auto;
+}
+
+.h-space-sm > *:not(:last-child) {
+  margin-right: 1vh;
+}
+
+.card {
+  font-size: 1.2em;
+  max-width: 500px;
+  transition: transform .75s;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card:hover {
   transform: scale(1.05);
 }
 
-.plugin-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
+.logo {
+  width: 50px;
 }
 
-.cover {
-  width: 75px;
-  margin-right: 20px;
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
 }
 
-.plugin-detail {
-  display: flex;
-  flex-direction: column;
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
 }
 
-.subtitle {
-  color: grey;
-  font-size: smaller;
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
 }
 
-.author {
-  color: #666;
-  font-style: italic;
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
