21a64d88f9d6507949c3b200efb728f219785e1a
Soli <asoliman96@gmail.com>
2026-07-29 16:29:13 +0000

    Version stable frontend assets

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -64,6 +64,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      hash: true,
       minify: {
         removeComments: true,
         collapseWhitespace: true,
